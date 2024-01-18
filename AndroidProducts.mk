d1673de1bad9509f47c9a0df11d761861d3ffc1e
YuichiGojo <ruinedmind2001@gmail.com>
2024-01-18 16:11:08 +0100

    Twrp Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_m51.mk
+    $(LOCAL_DIR)/twrp_m51.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_m51-user \
-    omni_m51-userdebug \
-    omni_m51-eng
+    twrp_m51-user \
+    twrp_m51-userdebug \
+    twrp_m51-eng
--- twrp_m51.mk
+++ twrp_m51.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common Twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from m51 device
 $(call inherit-product, device/samsung/m51/device.mk)
 
 PRODUCT_DEVICE := m51
-PRODUCT_NAME := omni_m51
+PRODUCT_NAME := twrp_m51
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M515F
 PRODUCT_MANUFACTURER := samsung
